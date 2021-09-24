0585c2ab9333e3dbbd42118b751bc44787e31652
ANDREYDEN <andrey2850@gmail.com>
2021-09-24 11:34:56 -0400

    Future builder

--- lib/main.dart
+++ lib/main.dart
@@ -30,51 +30,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _loading = true;
-  String _error = '';
-  Album? _data;
-
-  @override
-  void initState() {
-    _makeRequest('1');
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('API test')),
       body: Center(
-        child: _error != ''
-            ? Text(_error, style: const TextStyle(color: Colors.red))
-            : _loading
-                ? const CircularProgressIndicator()
-                : _data == null
-                    ? const Text('Nothing here yet...')
-                    : ListTile(title: Text('Title: ' + _data!.title), tileColor: Colors.amber,),
+        child: FutureBuilder<Album?>(
+          future: Api.getAlbum('-1'),
+          builder: (context, snap) {
+            if (snap.hasError) {
+              return Text(
+                snap.error.toString(),
+                style: const TextStyle(color: Colors.red),
+              );
+            }
+
+            if (snap.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+
+            Album album = snap.data!;
+
+            return ListTile(
+              title: Text('Title: ' + album.title),
+              tileColor: Colors.amber,
+            );
+          },
+        ),
       ),
     );
-  }
-
-  _makeRequest(String id) async {
-    setState(() {
-      _loading = true;
-      _error = '';
-    });
-
-    try {
-      Album? album = await Api.getAlbum(id);
-      setState(() {
-        _data = album;
-      });
-    } catch (e) {
-      setState(() {
-        _error = e.toString();
-      });
-    }
-
-    setState(() {
-      _loading = false;
-    });
   }
 }
